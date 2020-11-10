992a0df65e6d82ef1960db6852c47b7662095fbe
Kevinp3099 <20084163@mail.wit.ie>
2020-11-10 12:39:25 +0000

    Finally works except favorites

--- src/index.js
+++ src/index.js
@@ -2,37 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import MoviesContextProvider, {UpcomingMoviesContextProvider} from "./contexts/moviesContext";
 import HomePage from "./pages/homePage";
-import MoviePage from './pages/movieDetailsPage';
-import FavoriteMoviesPage from './pages/FavoritesMoviesPage';
+import MoviePage from './pages/movieDetailsPage'
+import FavoriteMoviesPage from './pages/favoritesMoviesPage'
+import UpcomingMoviesPage from './pages/upcomingMoviesPage'
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader';
-import UpcomingMovies from './pages/upcomingMoviesPage';
+import SiteHeader from './components/siteHeader'
+import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
-
 
 const App = () => {
   return (
     <BrowserRouter>
     <div className="jumbotron">
-          <SiteHeader /> 
-          <div className="container-fluid">
-          <MoviesContextProvider>
-          <UpcomingMoviesContextProvider>
-          <GenresContextProvider>
+    <SiteHeader />      {/* New Header  */}
+      <div className="container-fluid">
+       <MoviesContextProvider>
+       <GenresContextProvider>
         <Switch>
-          <Route exact path="/reviews/form" component={AddMovieReviewPage} />
-          <Route path="/reviews/:id" component={MovieReviewPage} />
+        <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+        <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-          <Route exact path="/movies/upcoming" component={upcomingMoviesPage} />
+          <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
           <Redirect from="*" to="/" />
         </Switch>
-        </GenresContextProvider>  
-        </UpcomingMoviesContextProvider>
+        </GenresContextProvider>
         </MoviesContextProvider>
       </div>
     </div>
