8076dd08d4e9f7aa576a3025862d92319ad29a89
Kevinp3099 <20084163@mail.wit.ie>
2020-11-12 12:41:18 +0000

    Fix upcoming movies page

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,23 +1,23 @@
-// import React, { useContext } from "react";
-// import PageTemplate from '../components/templateMovieListPage'
-// //import {UpcomingMoviesContext} from '../contexts/moviesContext'
-// import AddToFavoritesButton from '../components/buttons/addToFavorites'
+import React, { useContext } from "react";
+import PageTemplate from '../components/templateMovieListPage'
+import UpcomingContextProvider from '../contexts/upcomingMoviesContext'
+import AddToFavoritesButton from '../components/buttons/addToFavorites'
 
-// const MovieListPage = () => {
-//   const context = useContext(UpcomingMoviesContext);
-//   const movies = context.movies.filter((m) => {  // New
-//     return !("favorite" in m);
-//   });
+const UpcomingMoviesPage = () => {
+  const context = useContext(UpcomingContextProvider);
+  const movies = context.movies.filter((m) => {  // New
+    return !("favorite" in m);
+  });
 
-//   return (
-//     <PageTemplate
-//       title="New Movies!"
-//       movies={movies}  /* Changed */
-//       action={(movie) => {
-//         return <AddToFavoritesButton movie={movie} />;
-//       }}
-//     />
-//   );
-// };
+  return (
+    <PageTemplate
+      title="Upcoming Movies!"
+      movies={movies}  /* Changed */
+      action={(movie) => {
+        return <AddToFavoritesButton movie={movie} />;
+      }}
+    />
+  );
+};
 
-// export default MovieListPage;
+export default UpcomingMoviesPage;
