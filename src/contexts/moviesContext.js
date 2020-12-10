6562cd85da66d5eb4f2213b196f72e854782c4c2
Kevinp3099 <20084163@mail.wit.ie>
2020-12-10 15:12:48 +0000

    Fixing small watchlist error

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -38,7 +38,7 @@ const reducer = (state, action) => {
 
      case "add-watchList":
        return {
-         upcoming: state.movies.map((m) =>
+         upcoming: state.upcoming.map((m) =>
            m.id === action.payload.movie.id ? { ...m, watchList: true } : m
          ),
          movies: [...state.movies],
@@ -59,8 +59,8 @@ const MoviesContextProvider = (props) => {
   };
 
   const addToWatchList = (movieId) => {
-    const index = state.movies.map((m) => m.id).indexOf(movieId);
-    dispatch({ type: "add-watchList", payload: { movie: state.movies[index] } });
+    const index = state.upcoming.map((m) => m.id).indexOf(movieId);
+    dispatch({ type: "add-watchList", payload: { movie: state.upcoming[index] } });
   };
   const addReview = (movie, review) => {
     dispatch({ type: "add-review", payload: { movie, review } });
